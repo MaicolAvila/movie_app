7c0d219af3fc2d50ee03945f252136b0507a89ee
Angelica Romero <dallanaromero150@gmail.com>
2021-12-01 14:48:16 -0500

    vista registro

--- lib/pages/signup/SignUp.dart
+++ lib/pages/signup/SignUp.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:get/get.dart';
 import 'package:movie_app/controllers/email_create_controller.dart';
 import 'package:movie_app/flutter_flow/flutter_flow_theme.dart';
 import 'package:movie_app/flutter_flow/flutter_flow_widgets.dart';
 import 'package:movie_app/pages/home/Home.dart';
 import 'package:movie_app/pages/signin/SignIn.dart';
+import 'package:movie_app/theme/colors/cinapp_colors.dart';
 
 class SignUp extends StatefulWidget {
   SignUp({Key? key}) : super(key: key);
@@ -37,73 +39,166 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     final emailController = Get.put(EmailCreateController());
+
     return Scaffold(
       key: scaffoldKey,
-      appBar: AppBar(
-        backgroundColor: FlutterFlowTheme.primaryColor,
-        automaticallyImplyLeading: true,
-        actions: [],
-        centerTitle: true,
-        elevation: 4,
-      ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: EdgeInsetsDirectional.fromSTEB(20, 40, 20, 0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: CinappColors.purple1),
+        child: Stack(
+          children: [
+            Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                child:
+                    Image.asset('assets/login_vector.png', fit: BoxFit.fill)),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Obx(
-                  () => Visibility(
-                    visible: emailController.isLoading.value,
-                    child: const Center(child: CircularProgressIndicator()),
-                  ),
-                ),
-                Obx(
-                  () => Visibility(
-                    visible: emailController.error.value?.isNotEmpty == true,
-                    child: Text(
-                      emailController.error.value ?? '',
-                      style: const TextStyle(color: Colors.red, fontSize: 24),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: emailController.emailController,
-                  decoration: const InputDecoration(labelText: 'Email'),
-                  validator: emailController.emailValidator,
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: emailController.passwordController,
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  validator: emailController.passwordValidator,
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: emailController.repeatPasswordController,
-                  decoration:
-                      const InputDecoration(labelText: 'Repeat Password'),
-                  validator: emailController.passwordValidator,
-                ),
-                Center(
-                  child: ElevatedButton(
-                    child: const Text('Create'),
-                    onPressed: () {
-                      if (_formKey.currentState?.validate() == true) {
-                        Get.find<EmailCreateController>()
-                            .createUserWithEmailAndPassword();
-                      }
+                Form(
+                  child: KeyboardVisibilityBuilder(
+                    builder: (context, visible) {
+                      return Container(
+                        height: visible
+                            ? MediaQuery.of(context).size.height * 0.8
+                            : MediaQuery.of(context).size.height * 0.5,
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Obx(
+                              () => Visibility(
+                                visible: emailController.isLoading.value,
+                                child: const Center(
+                                    child: CircularProgressIndicator()),
+                              ),
+                            ),
+                            Obx(
+                              () => Visibility(
+                                visible:
+                                    emailController.error.value?.isNotEmpty ==
+                                        true,
+                                child: Text(
+                                  emailController.error.value ?? '',
+                                  style: const TextStyle(
+                                      color: Colors.red, fontSize: 24),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Registrarse",
+                                    style: TextStyle(
+                                        color: CinappColors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 36),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: Column(
+                                children: [
+                                  const SizedBox(height: 8),
+                                  TextFormField(
+                                    controller: emailController.emailController,
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      labelText: 'Email',
+                                      labelStyle:
+                                          TextStyle(color: CinappColors.white),
+                                      filled: true,
+                                      fillColor: CinappColors.purple2,
+                                    ),
+                                    validator: emailController.emailValidator,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
+                                    controller:
+                                        emailController.passwordController,
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      labelText: 'Password',
+                                      labelStyle:
+                                          TextStyle(color: CinappColors.white),
+                                      filled: true,
+                                      fillColor: CinappColors.purple2,
+                                    ),
+                                    validator:
+                                        emailController.passwordValidator,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
+                                    controller: emailController
+                                        .repeatPasswordController,
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      labelText: 'Password',
+                                      labelStyle:
+                                          TextStyle(color: CinappColors.white),
+                                      filled: true,
+                                      fillColor: CinappColors.purple2,
+                                    ),
+                                    validator:
+                                        emailController.passwordValidator,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: Column(
+                                children: [
+                                  ElevatedButton(
+                                    child: const Text(
+                                      'Registrar',
+                                      style: TextStyle(
+                                          color: CinappColors.purple1,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: CinappColors.sky1,
+                                      fixedSize: Size(
+                                          MediaQuery.of(context).size.width,
+                                          50),
+                                    ),
+                                    onPressed: () {
+                                      if (_formKey.currentState?.validate() ==
+                                          true) {
+                                        Get.find<EmailCreateController>()
+                                            .createUserWithEmailAndPassword();
+                                      }
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
                     },
                   ),
                 ),
               ],
             ),
-          ),
+          ],
         ),
       ),
     );
