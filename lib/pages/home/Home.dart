f3ddf110107ce097bcadc460e2b386981d65e341
Angelica Romero <dallanaromero150@gmail.com>
2021-12-01 22:16:14 -0500

    home

--- lib/pages/home/Home.dart
+++ lib/pages/home/Home.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:movie_app/components/buttonnav.dart';
+import 'package:movie_app/components/card_home.dart';
 import 'package:movie_app/repository/auth_repository.dart';
 import 'package:movie_app/theme/colors/cinapp_colors.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
+
+  main() {
+    var list = [titulo, descripcion, imagen];
+    print(list is List); // true
+  }
 
   @override
   _HomeState createState() => _HomeState();
@@ -20,169 +26,41 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     final _authRepository = Get.find<AuthRepository>();
     return Scaffold(
-        body: Container(
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(color: CinappColors.purple1),
-            child: Stack(children: [
-              Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  child:
-                      Image.asset('assets/vector-home.png', fit: BoxFit.fill)),
-              Column(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.all(99),
-                    child: Column(
-                      children: [
-                        Text(
-                          "Recomendaciones",
-                          style: TextStyle(
-                              color: CinappColors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                        ),
-                      ],
-                    ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: CinappColors.purple1),
+        child: Stack(
+          children: [
+            Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                child: Image.asset('assets/vector-home.png', fit: BoxFit.fill)),
+            Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.all(99),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Recomendaciones",
+                        style: TextStyle(
+                            color: CinappColors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ],
                   ),
-                  Container(
-                    decoration: new BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: CinappColors.purple3),
-                    width: 350,
-                    height: 144,
-                    margin: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "  El Señor de los anillos\n",
-                              style: TextStyle(
-                                  color: CinappColors.white, fontSize: 15),
-                            ),
-                            Container(
-                              width: 350 * 0.5,
-                              child: Text(
-                                " El Señor Oscuro Sauron ordenó forjar una serie de anillos de poder; tres para los reyes...",
-                                style: TextStyle(
-                                    color: CinappColors.white, fontSize: 15),
-                                overflow: TextOverflow.clip,
-                              ),
-                            )
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Image.asset('assets/pelicula1.png',
-                                fit: BoxFit.fill),
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(Icons.star, color: Colors.green[500]),
-                                Icon(Icons.star, color: Colors.green[500]),
-                                Icon(Icons.star, color: Colors.green[500]),
-                                Icon(Icons.star, color: Colors.black),
-                                Icon(Icons.star, color: Colors.black),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    decoration: new BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: CinappColors.purple3),
-                    width: 350,
-                    height: 144,
-                    margin: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "  El Señor de los anillos\n",
-                              style: TextStyle(
-                                  color: CinappColors.white, fontSize: 15),
-                            ),
-                            Text(
-                              " El Señor Oscuro \n Sauron ordenó \n forjar una serie \n de anillos de poder; \n tres para los reyes...",
-                              style: TextStyle(
-                                  color: CinappColors.white, fontSize: 15),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Image.asset('assets/pelicula1.png',
-                                fit: BoxFit.fill),
-                          ],
-                        ),
-                        Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(Icons.star, color: Colors.green[500]),
-                            Icon(Icons.star, color: Colors.green[500]),
-                            Icon(Icons.star, color: Colors.green[500]),
-                            Icon(Icons.star, color: Colors.black),
-                            Icon(Icons.star, color: Colors.black),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    decoration: new BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: CinappColors.purple3),
-                    width: 350,
-                    height: 144,
-                    margin: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "  El Señor de los anillos\n",
-                              style: TextStyle(
-                                  color: CinappColors.white, fontSize: 15),
-                            ),
-                            Text(
-                              " El Señor Oscuro \n Sauron ordenó \n forjar una serie \n de anillos de poder; \n tres para los reyes...",
-                              style: TextStyle(
-                                  color: CinappColors.white, fontSize: 15),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Image.asset('assets/pelicula1.png',
-                                fit: BoxFit.fill),
-                          ],
-                        ),
-                        Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(Icons.star, color: Colors.green[500]),
-                            Icon(Icons.star, color: Colors.green[500]),
-                            Icon(Icons.star, color: Colors.green[500]),
-                            Icon(Icons.star, color: Colors.black),
-                            Icon(Icons.star, color: Colors.black),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  ButtonNav()
-                ],
-              ),
-            ])));
+                ),
+                CardHome(),
+                CardHome(),
+                ButtonNav()
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
