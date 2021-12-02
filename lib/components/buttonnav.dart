11df7cfffd0f1c73965ba134bd389e1ed06c3761
MaicolAvila <maicol.2001@hotmail.com>
2021-12-01 22:37:26 -0500

    ButtonNav icons and with singOut function ready!

--- lib/components/buttonnav.dart
+++ lib/components/buttonnav.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:movie_app/icons/cinapp_icons_icons.dart';
+import 'package:movie_app/repository/auth_repository.dart';
 import 'package:movie_app/theme/colors/cinapp_colors.dart';
 
 class ButtonNav extends StatelessWidget {
@@ -7,51 +10,76 @@ class ButtonNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
+    final _authRepository = Get.find<AuthRepository>();
+    return Positioned.fill(
         bottom: 0,
-        left: 0,
-        right: 0,
-        height: 50,
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.8,
-          child: Column(
-            children: [
-              Container(
-                height: 2,
-                color: CinappColors.sky1,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Icon(
-                    CinappIcons.message_ico,
+                  Container(
+                    height: 2,
                     color: CinappColors.sky1,
-                    size: 30,
                   ),
-                  Icon(
-                    CinappIcons.maki_cinema_ico,
-                    color: CinappColors.sky1,
-                    size: 30,
+                  SizedBox(
+                    height: 10,
                   ),
-                  Icon(
-                    CinappIcons.map_pin_ico,
-                    color: CinappColors.sky1,
-                    size: 30,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () => {},
+                        icon: Icon(
+                          CinappIcons.message_ico,
+                          color: CinappColors.sky1,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => {},
+                        icon: Icon(
+                          CinappIcons.maki_cinema_ico,
+                          color: CinappColors.sky1,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => {},
+                        icon: Icon(
+                          CinappIcons.map_pin_ico,
+                          color: CinappColors.sky1,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => {},
+                        icon: Icon(
+                          CinappIcons.bi_people_ico,
+                          color: CinappColors.sky1,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => {_authRepository.signOut()},
+                        icon: Icon(
+                          CinappIcons.signout_ico,
+                          color: CinappColors.sky1,
+                          size: 30,
+                        ),
+                      )
+                    ],
                   ),
-                  Icon(
-                    CinappIcons.bi_people_ico,
-                    color: CinappColors.sky1,
-                    size: 30,
-                  ),
-                  Icon(
-                    CinappIcons.signout_ico,
-                    color: CinappColors.sky1,
-                    size: 30,
+                  SizedBox(
+                    height: 10,
                   ),
                 ],
-              )
-            ],
-          ),
-        ));
+              ),
+            )));
   }
 }
