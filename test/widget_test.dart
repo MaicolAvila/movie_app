b40ebfca475708bffc07d92cfe6d4cfaa18a112b
MaicolAvila <maicol.2001@hotmail.com>
2021-12-01 23:28:13 -0500

    test tests :v

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,35 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:movie_app/app.dart';
+import 'package:movie_app/pages/signin/SignIn.dart';
+import 'package:get/get.dart';
+import 'package:get_test/get_test.dart';
+import 'package:movie_app/pages/signup/SignUp.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  getTest(
+    "test description",
+    widgetTest: (tester) async {
+      expect('/', Get.currentRoute);
+
+      Get.to(SignIn());
+      expect('/signInEmail', Get.currentRoute);
+
+      Get.to(SignUp());
+      expect('/createAccount', Get.currentRoute);
+
+      Get.back();
+      expect('/signInEmail', Get.currentRoute);
+    },
+  );
+  testWidgets('Login has a title', (WidgetTester tester) async {
+    // Test code goes here.
+    await tester.pumpWidget(SignIn());
+    final titleLogin = find.text('Login');
+
+    expect(titleLogin, findsOneWidget);
   });
 }
